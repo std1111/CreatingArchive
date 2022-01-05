5819ca785ba54b08cd80e8d7d2dfdcb5dcf089d7
gennady.matyuk <gennady.matyuk@qatestlab.com>
2022-01-05 02:31:38 +0200

    Initial commit

---
+++ BackUp_WEB100.bat
@@ -0,0 +1,3 @@
+Ñ:
+CD C:\BackUp\Tasks
+Start python.exe "C:\BackUp\Tasks\CreatingArchive\CreateAndSyncArhive.py" --BASE_DIR "C:\Bases\baza_tovarov" -ARCH_DIR "C:\BackUp\Baza" --PREFiX_FILE_ARCH "Baza" --DRIVE_FOLDER_ID "1kkWAy0WqXKXkp7fU8lrr3LAXhexbft4z"
